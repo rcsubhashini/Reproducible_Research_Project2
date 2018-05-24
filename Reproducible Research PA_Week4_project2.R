7214ed1936b2bef893f5f0ac2875130ff1d58907
rcsubhashini <33597781+rcsubhashini@users.noreply.github.com>
2018-05-24 15:13:08 +0530

    Add files via upload

---
+++ Reproducible Research PA_Week4_project2.R
@@ -0,0 +1,84 @@
+#Your data analysis must address the following questions:
+  
+# 1.Across the United States, which types of events (as indicated in the \color{red}{\verb|EVTYPE|}EVTYPE variable) are most harmful with respect to population health?
+# 2.Across the United States, which types of events have the greatest economic consequences?
+# 
+# Consider writing your report as if it were to be read by a government or municipal manager who might be responsible for preparing for severe weather events and will need to prioritize resources for different types of events. 
+# However, there is no need to make any specific recommendations in your report.
+
+
+
+# 1.Across the United States, which types of events (as indicated in the \color{red}{\verb|EVTYPE|}EVTYPE variable) are most harmful with respect to population health?
+library(dplyr)
+storm_data_file <- read.csv("C:\\Users\\d.a.ganesan\\Desktop\\repdata_Fdata_FStormData.csv")
+head(storm_data_file)
+dim(storm_data_file)
+
+storm_data <- storm_data_file[ , c(8, 23:28)]
+rm(storm_data_file)
+head(storm_data)
+
+# printing the brief summary of FATALITIES and INJURIES
+summary(storm_data$FATALITIES)
+summary(storm_data$INJURIES)
+
+# aggregate the fatalities and injuries by types of events, order descending and subset the top 20
+total_injuries <- aggregate(INJURIES~EVTYPE, storm_data, sum)
+total_injuries <- arrange(total_injuries, desc(INJURIES))
+total_injuries <- total_injuries[1:20, ]
+total_injuries
+
+total_fatalities <- aggregate(FATALITIES~EVTYPE, storm_data, sum)
+total_fatalities <- arrange(total_fatalities, desc(FATALITIES))
+total_fatalities <- total_fatalities[1:20, ]
+total_fatalities
+
+# Results
+# print the results, we merge fatalities and injuries in one data set
+
+totals<- merge(total_fatalities, total_injuries, by.x = "EVTYPE", by.y = "EVTYPE")
+totals<-arrange(totals,desc(FATALITIES+INJURIES))
+names_events <- totals$EVTYPE
+barplot(t(totals[,-1]), names.arg = names_events, beside = T, cex.names = 0.8, las=2, col = c("light blue", "pink"), main="Top Disaster Casualties")
+legend("topright",c("Fatalities","Injuries"),fill=c("light blue","pink"),bty = "n")
+
+# 2.Across the United States, which types of events have the greatest economic consequences?
+
+## Data Processing
+storm_data$PROPDAMAGE = 0
+storm_data[storm_data$PROPDMGEXP == "H", ]$PROPDAMAGE = storm_data[storm_data$PROPDMGEXP == "H", ]$PROPDMG * 10^2
+storm_data[storm_data$PROPDMGEXP == "K", ]$PROPDAMAGE = storm_data[storm_data$PROPDMGEXP == "K", ]$PROPDMG * 10^3
+storm_data[storm_data$PROPDMGEXP == "M", ]$PROPDAMAGE = storm_data[storm_data$PROPDMGEXP == "M", ]$PROPDMG * 10^6
+storm_data[storm_data$PROPDMGEXP == "B", ]$PROPDAMAGE = storm_data[storm_data$PROPDMGEXP == "B", ]$PROPDMG * 10^9
+
+storm_data$CROPDAMAGE = 0
+storm_data[storm_data$CROPDMGEXP == "H", ]$CROPDAMAGE = storm_data[storm_data$CROPDMGEXP == "H", ]$CROPDMG * 10^2
+storm_data[storm_data$CROPDMGEXP == "K", ]$CROPDAMAGE = storm_data[storm_data$CROPDMGEXP == "K", ]$CROPDMG * 10^3
+storm_data[storm_data$CROPDMGEXP == "M", ]$CROPDAMAGE = storm_data[storm_data$CROPDMGEXP == "M", ]$CROPDMG * 10^6
+storm_data[storm_data$CROPDMGEXP == "B", ]$CROPDAMAGE = storm_data[storm_data$CROPDMGEXP == "B", ]$CROPDMG * 10^9
+
+# Aggregate property and crop damage into one variable. Arrange and select the top 20.
+
+economic_damage <- aggregate(PROPDAMAGE + CROPDAMAGE ~ EVTYPE, storm_data, sum)
+names(economic_damage) = c("EVENT_TYPE", "TOTAL_DAMAGE")
+economic_damage <- arrange(economic_damage, desc(TOTAL_DAMAGE))
+economic_damage <- economic_damage[1:20, ]
+economic_damage$TOTAL_DAMAGE <- economic_damage$TOTAL_DAMAGE/10^9
+economic_damage$EVENT_TYPE <- factor(economic_damage$EVENT_TYPE, levels = economic_damage$EVENT_TYPE)
+head(economic_damage)
+
+## Results
+with(economic_damage, barplot(TOTAL_DAMAGE, names.arg = EVENT_TYPE, beside = T, cex.names = 0.8, las=2, col = "light green", main = "Total Property and Crop Damage by Top 20 Event Types", ylab = "Total Damage in USD (10^9)"))
+
+
+
+
+
+
+
+
+
+
+
+
+
